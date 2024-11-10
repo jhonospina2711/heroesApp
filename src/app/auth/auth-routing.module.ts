ebcf6dba1f04d542aa03a6eff4d72eef22905fc4
Jhon Ospina <jhonospina2711@gmail.com>
2024-11-10 01:31:18 -0500

    Se crean todas las rutas necesarias para el proyecto heroesApp

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPagesComponent,
+    children: [
+      {path: 'login', component: LoginPageComponent},
+      {path: 'new-account', component: RegisterPageComponent},
+      {path: '**', redirectTo: 'login'},
+    ]
+  }
+];
 
 @NgModule({
   imports: [ RouterModule.forChild( routes )],
